e7a26b03a3bf1ba6f680665c2176cf2c073003bf
yandirad <yandirad@gmail.com>
2021-10-28 19:20:40 -0300

    Se creo un paquete de excepciones

---
+++ demo/src/main/java/com/calendario_siembra/demo/exceptions/WebException.java
@@ -0,0 +1,12 @@
+
+package com.calendario_siembra.demo.exceptions;
+
+
+public class WebException extends Exception {
+    
+    public WebException(String msj){
+        super(msj);
+    }
+
+    
+}
