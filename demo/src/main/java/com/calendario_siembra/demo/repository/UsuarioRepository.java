bd84eba566d8a3688603d422beea9d62511c43f2
ramirocabrera <ramirocabrera1991@gmail.com>
2021-10-28 21:41:10 -0300

    Se modifico el nombre del metodo en UsuarioRepository

--- demo/src/main/java/com/calendario_siembra/demo/repository/UsuarioRepository.java
+++ demo/src/main/java/com/calendario_siembra/demo/repository/UsuarioRepository.java
@@ -1,20 +1,19 @@
 
 package com.calendario_siembra.demo.repository;
 
-import com.calendario_siembra.demo.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.calendario_siembra.demo.entity.Usuario;
+
 /**
  *
- * Si hya fallas, probar de reemplazar 'JpaRepository<Usuario, String>' 
- * por 'CrudRepository<Usuario, String>'
+ * Si hya fallas, probar de reemplazar 'JpaRepository<Usuario, String>' por
+ * 'CrudRepository<Usuario, String>'
  */
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
-    
-    
-    public Usuario findByUsername(String username);
-        
-    
+
+	public Usuario findByUsuario(String username);
+
 }
