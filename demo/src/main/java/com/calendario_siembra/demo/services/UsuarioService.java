2d0e3dc1ac7b577d638a1ec5770646863337d648
ramirocabrera <ramirocabrera1991@gmail.com>
2021-11-01 17:09:04 -0300

    Correccion faltante despues de la ultima modificacion en la invocacion al metodo del repositorio para buscar un usuario

--- demo/src/main/java/com/calendario_siembra/demo/services/UsuarioService.java
+++ demo/src/main/java/com/calendario_siembra/demo/services/UsuarioService.java
@@ -6,12 +6,11 @@
 
 package com.calendario_siembra.demo.services;
 
-import com.calendario_siembra.demo.entity.Usuario;
-import com.calendario_siembra.demo.exceptions.WebException;
-import com.calendario_siembra.demo.repository.UsuarioRepository;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,56 +19,60 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.calendario_siembra.demo.entity.Usuario;
+import com.calendario_siembra.demo.exceptions.WebException;
+import com.calendario_siembra.demo.repository.UsuarioRepository;
+
 /**
  *
  * @author yandirad
  */
 @Service
 public class UsuarioService implements UserDetailsService {
-    
-    @Autowired
-    private UsuarioRepository usuarioRepository;
 
-    //asigna al usuario el rol de USUARIO sin permisos especiales
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { 
-        Usuario user = usuarioRepository.findByUsername(username); 
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-    
-        // use list if you wish 
-        grantedAuthorities.add(new SimpleGrantedAuthority(user.getRol()));
-        return new org.springframework.security.core.userdetails.User(user.getUsuario(), user.getPass(), 
-                grantedAuthorities); } 
-    
-    @Transactional
-    public Usuario buscarUsuario(String nombre) { 
-        return usuarioRepository.findByUsername(nombre); 
-    }
-    
-    @Transactional
-    public void guardarUsuario(Usuario usuario) throws WebException{
-        
-        validar(usuario);
-        usuarioRepository.save(usuario);
- 
-    }
-    
-    
-    public void validar(Usuario usuario) throws WebException{
-        
-        if(usuario.getNombre().isEmpty() || usuario.getNombre().equals("") || usuario.getNombre()==null){
-            throw new WebException("El nombre no puede estar vacío");
-        }
-        if(usuario.getUsuario().isEmpty() || usuario.getUsuario().equals("") || usuario.getUsuario()==null){
-            throw new WebException("El usuario no puede estar vacío");
-        }
-        if(usuario.getPass().isEmpty() || usuario.getPass().equals("") || usuario.getPass()==null){
-            throw new WebException("La contraseña no puede estar vacía");
-        }
-        if(usuario.getMail().isEmpty() || usuario.getMail().equals("") || usuario.getMail()==null){
-            throw new WebException("El mail no puede estar vacío");
-        }
-        
-    }
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	// asigna al usuario el rol de USUARIO sin permisos especiales
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario user = usuarioRepository.findByUsuario(username);
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+		// use list if you wish
+		grantedAuthorities.add(new SimpleGrantedAuthority(user.getRol()));
+		return new org.springframework.security.core.userdetails.User(user.getUsuario(), user.getPass(),
+				grantedAuthorities);
+	}
+
+	@Transactional
+	public Usuario buscarUsuario(String nombre) {
+		return usuarioRepository.findByUsuario(nombre);
+	}
+
+	@Transactional
+	public void guardarUsuario(Usuario usuario) throws WebException {
+
+		validar(usuario);
+		usuarioRepository.save(usuario);
+
+	}
+
+	public void validar(Usuario usuario) throws WebException {
+
+		if (usuario.getNombre().isEmpty() || usuario.getNombre().equals("") || usuario.getNombre() == null) {
+			throw new WebException("El nombre no puede estar vacío");
+		}
+		if (usuario.getUsuario().isEmpty() || usuario.getUsuario().equals("") || usuario.getUsuario() == null) {
+			throw new WebException("El usuario no puede estar vacío");
+		}
+		if (usuario.getPass().isEmpty() || usuario.getPass().equals("") || usuario.getPass() == null) {
+			throw new WebException("La contraseña no puede estar vacía");
+		}
+		if (usuario.getMail().isEmpty() || usuario.getMail().equals("") || usuario.getMail() == null) {
+			throw new WebException("El mail no puede estar vacío");
+		}
+
+	}
 
 }
