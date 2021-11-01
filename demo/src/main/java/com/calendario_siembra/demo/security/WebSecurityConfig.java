6f9abe59f81012f185d5e0bfc16658e725002f84
ramirocabrera <ramirocabrera1991@gmail.com>
2021-11-01 19:13:52 -0300

    Se completo lo que faltaba para la seguridad de la web

--- demo/src/main/java/com/calendario_siembra/demo/security/WebSecurityConfig.java
+++ demo/src/main/java/com/calendario_siembra/demo/security/WebSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.calendario_siembra.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.calendario_siembra.demo.services.UsuarioService;
 
 @Configuration
 @EnableWebSecurity
@@ -38,17 +42,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return bCryptPasswordEncoder;
 	}
 
-	/*
-	 * @Autowired UserDetailsServiceImpl userDetailsService;
-	 * 
-	 * // Registra el service para usuarios y el encriptador de contrasena
-	 * 
-	 * @Autowired public void configureGlobal(AuthenticationManagerBuilder auth)
-	 * throws Exception {
-	 * 
-	 * // Setting Service to find User in the database. // And Setting
-	 * PassswordEncoder
-	 * auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()
-	 * ); }
-	 */
+	@Autowired
+	UsuarioService usuarioService;
+
+	// Registra el service para usuarios y el encriptador de contrasena
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// Setting Service to find User in the database.
+		// And Setting PassswordEncoder
+		auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
+	}
+
 }
