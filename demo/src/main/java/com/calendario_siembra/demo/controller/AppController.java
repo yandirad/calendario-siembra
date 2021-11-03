893de01ed269305a7b9a560b2d2858e0b14c2445
ramirocabrera <ramirocabrera1991@gmail.com>
2021-11-02 22:01:52 -0300

    Se agrego al controlador el modelo sobre el index.html

--- demo/src/main/java/com/calendario_siembra/demo/controller/AppController.java
+++ demo/src/main/java/com/calendario_siembra/demo/controller/AppController.java
@@ -1,21 +1,29 @@
 package com.calendario_siembra.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.calendario_siembra.demo.services.PlantaService;
 
 @Controller
 @RequestMapping("/")
 public class AppController {
 
+	@Autowired
+	PlantaService plantaService;
+
 	@GetMapping({ "/", "/index" })
-	public String index() {
-            return "index.html";
+	public String index(Model modelo) {
+		modelo.addAttribute("listaPlantas", plantaService.listarPlantas());
+		return "index.html";
 	}
 
 	@GetMapping("/login")
 	public String login() {
-            return "login.html";
+		return "login.html";
 	}
 
 }
