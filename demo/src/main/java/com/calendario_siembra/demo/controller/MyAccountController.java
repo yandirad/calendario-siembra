063510df35aa76a604eb910cee644326f1b134fe
ramirocabrera <ramirocabrera1991@gmail.com>
2021-11-03 21:40:43 -0300

    Se completaron metodos y se corrigieron returns en base a lo que se fue necesitando por el html

--- demo/src/main/java/com/calendario_siembra/demo/controller/MyAccountController.java
+++ demo/src/main/java/com/calendario_siembra/demo/controller/MyAccountController.java
@@ -6,20 +6,21 @@
 
 package com.calendario_siembra.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.calendario_siembra.demo.entity.Parcela;
 import com.calendario_siembra.demo.entity.Planta;
 import com.calendario_siembra.demo.entity.Usuario;
 import com.calendario_siembra.demo.exceptions.WebException;
 import com.calendario_siembra.demo.services.ParcelaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.calendario_siembra.demo.services.PlantaService;
 import com.calendario_siembra.demo.services.UsuarioService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/my-account")
@@ -27,83 +28,89 @@ public class MyAccountController {
 
 	@Autowired
 	UsuarioService usuarioService;
-        
-        @Autowired
-        ParcelaService parcelaService;
 
-        //vista inicial del perfil de cada usuario
+	@Autowired
+	ParcelaService parcelaService;
+
+	@Autowired
+	PlantaService plantaService;
+
+	// vista inicial del perfil de cada usuario
 	@GetMapping("/")
 	public String verDatos(Model modelo) {
-		modelo.addAttribute("usuario", usuarioService.buscarUsuario());
-
+		Usuario usuario = usuarioService.buscarUsuario();
+		if (usuario == null)
+			return "redirect:/login";
+		modelo.addAttribute("usuario", usuario);
+		modelo.addAttribute("nuevaParcela", new Parcela(usuario));
+		modelo.addAttribute("listaCultivos", plantaService.listarPlantas());
+		modelo.addAttribute("nuevaPlanta", new Planta());
 		return "myaccount.html";
 	}
-        
-        //Metodo para modificar datos del usuario, mediante pop-up
-        @PostMapping("/usuario-modificar")
-        public String modificarUsuario(@RequestParam Usuario usuario, Model modelo) {
-            try {
-                usuarioService.guardarUsuario(usuario);
 
-            } catch (WebException e) {
-                modelo.addAttribute("error", e.getMessage());
-            }
-            
-            return "redirect:/myaccount.html/"; 
-        }
-        
-        //Método para crear/modificar una nueva parcela
-        @PostMapping("/parcela-alta-modificar")
-        public String crearParcela(@RequestParam(required=false) Parcela parcela, Model modelo) {
-            try {
-                if (parcela.getId() == null) {
-                    parcelaService.crearParcela(parcela);
-                } else {
-                    parcelaService.modificarParcela(parcela);
-                }
-            } catch (WebException e) {
-                modelo.addAttribute("error", e.getMessage());
-            }
-   
-            return "redirect:/myaccount.html/";
-        }
-        
-        //Método para dar la baja a una parcela
-        @PostMapping("/parcela-baja")
-        public String bajaParcela(@RequestParam Parcela parcela, Model modelo) {
-            try {
-                parcelaService.bajaParcela(parcela);   
-            } catch (WebException e) {
-                modelo.addAttribute("error", e.getMessage());
-            }
-   
-            return "redirect:/myaccount.html/";
-        }
-        
-      
-      //Método para dar la alta a una planta
-        @PostMapping("/planta-alta")
-        public String altaPlanta(@RequestParam Planta planta,@RequestParam Parcela parcela, Model modelo) {
-            try {
-                parcelaService.agregarPlanta(parcela, planta);  
-            } catch (WebException e) {
-                modelo.addAttribute("error", e.getMessage());
-            }
-   
-            return "redirect:/myaccount.html/";
-        }
-        
-        //Método para dar la baja a una planta
-        @PostMapping("/planta-baja")
-        public String bajaPlanta(@RequestParam Planta planta,@RequestParam Parcela parcela, Model modelo) {
-            try {
-                parcelaService.bajaPlanta(parcela,planta);   
-            } catch (WebException e) {
-                modelo.addAttribute("error", e.getMessage());
-            }
-   
-            return "redirect:/myaccount.html/";
-        }
-        
-        
+	// Metodo para modificar datos del usuario, mediante pop-up
+	@PostMapping("/usuario-modificar")
+	public String modificarUsuario(@RequestParam Usuario usuario, Model modelo) {
+		try {
+			usuarioService.guardarUsuario(usuario);
+
+		} catch (WebException e) {
+			modelo.addAttribute("error", e.getMessage());
+		}
+
+		return "redirect:/my-account.html/";
+	}
+
+	// Método para crear/modificar una nueva parcela
+	@PostMapping("/parcela-alta-modificar")
+	public String crearParcela(Parcela parcela, Model modelo) {
+		try {
+			if (parcela.getId() == null) {
+				parcelaService.crearParcela(parcela);
+			} else {
+				parcelaService.modificarParcela(parcela);
+			}
+		} catch (WebException e) {
+			modelo.addAttribute("error", e.getMessage());
+		}
+
+		return "redirect:/my-account/";
+	}
+
+	// Método para dar la baja a una parcela
+	@PostMapping("/parcela-baja")
+	public String bajaParcela(@RequestParam Parcela parcela, Model modelo) {
+		try {
+			parcelaService.bajaParcela(parcela);
+		} catch (WebException e) {
+			modelo.addAttribute("error", e.getMessage());
+		}
+
+		return "redirect:/my-account.html/";
+	}
+
+	// Método para dar la alta a una planta
+	@PostMapping("/planta-alta")
+	public String altaPlanta(@RequestParam Planta planta, @RequestParam Parcela parcela, Model modelo) {
+		try {
+			parcelaService.agregarPlanta(parcela, planta);
+		} catch (WebException e) {
+			modelo.addAttribute("error", e.getMessage());
+		}
+
+		return "redirect:/my-account.html/";
+	}
+
+	// Método para dar la baja a una planta
+	@PostMapping("/planta-baja")
+	public String bajaPlanta(@RequestParam Planta planta, @RequestParam Parcela parcela, Model modelo) {
+		try {
+			parcelaService.bajaPlanta(parcela, planta);
+		} catch (WebException e) {
+			modelo.addAttribute("error", e.getMessage());
+		}
+
+		return "redirect:/my-account.html/";
+	}
+
 }
