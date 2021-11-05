5028bc06c01b0e5ad7315d37502eaacc2783d7e4
ramirocabrera <ramirocabrera1991@gmail.com>
2021-11-05 14:09:34 -0300

    Se agrega en mapping logout, invocacion a metodo se seguridad para limpiar la sesion

--- demo/src/main/java/com/calendario_siembra/demo/controller/AppController.java
+++ demo/src/main/java/com/calendario_siembra/demo/controller/AppController.java
@@ -1,14 +1,15 @@
 package com.calendario_siembra.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.calendario_siembra.demo.services.PlantaService;
 import com.calendario_siembra.demo.services.UsuarioService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -16,8 +17,8 @@ public class AppController {
 
 	@Autowired
 	PlantaService plantaService;
-        
-        @Autowired
+
+	@Autowired
 	UsuarioService usuarioService;
 
 	@GetMapping({ "/", "/index" })
@@ -27,21 +28,21 @@ public class AppController {
 	}
 
 	@GetMapping("/login")
-	public String login(Model model, @RequestParam(required=false) String error, 
-                @RequestParam(required=false) String usuario) {
-            if(error != null){
-                model.addAttribute("error", "El usuario ingresado o la contraseña no son válidos.");
-            }
-            if(usuario != null){
-                model.addAttribute("usuario", usuario);
-            }
-            return "login.html";
+	public String login(Model model, @RequestParam(required = false) String error,
+			@RequestParam(required = false) String usuario) {
+		if (error != null) {
+			model.addAttribute("error", "El usuario ingresado o la contraseña no son válidos.");
+		}
+		if (usuario != null) {
+			model.addAttribute("usuario", usuario);
+		}
+		return "login.html";
 	}
-        
-        @GetMapping("/logout")
+
+	@GetMapping("/logout")
 	public String logout(Model model) {
-            model.addAttribute("success", "Ha finalizado su sesión exitosamente.");
-   
-            return "login";
+		model.addAttribute("success", "Ha finalizado su sesión exitosamente.");
+		SecurityContextHolder.clearContext();
+		return "login";
 	}
 }
